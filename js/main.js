ca12d1df78c31bc9dbc83232c31fad7ee9efea88
Marat Khachatryan <maratkhachatryan97@gmail.com>
2020-10-20 23:57:35 +0400

    Uploaded files for website(WIP)

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(".menu").click(function (e) {
+	e.preventDefault(), $(this).toggleClass("is-active"),
+		$(".nav").toggleClass("opened"), $("html").toggleClass("overflow");
+	$(".nav__link").click(function (e) {
+		e.preventDefault();
+		if ($(window).width() <= 768) {
+			$(".header__nav").removeClass("opened");
+			$("html").removeClass("overflow");
+			$(".menu").removeClass("is-active");
+		}
+	})
+})
+jQuery(document).ready(($) => {
+	$('.accordion__block').click(function () {
+		$(this).find('.accordion__close').toggleClass('open');
+		$(this)
+			.next('.accordion__open')
+			.slideToggle('fast', () => {
+				// Do something maybe ..
+			});
+	});
+
+	$('.question__block').click(function () {
+		let container = $(this).parents('.question');
+		let answer = container.find('.question__answer');
+		let trigger = container.find('.question__arrow');
+
+		answer.slideToggle(200);
+
+		if (trigger.hasClass('question__rotated')) {
+			trigger.removeClass('question__rotated');
+		} else {
+			trigger.addClass('question__rotated');
+		}
+
+		if (container.hasClass('expanded')) {
+			container.removeClass('expanded');
+		} else {
+			container.addClass('expanded');
+		}
+	})
+})
+
+
+
+
+
