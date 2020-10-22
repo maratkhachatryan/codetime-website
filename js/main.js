648bd0c54591245a178cce1a09e2cece604b978f
“Aleksandr <ren_alex@mail.ru>
2020-10-22 17:15:49 +0400

    Finished website up to 320px, Hoorraygit add .!

--- js/main.js
+++ js/main.js
@@ -41,6 +41,17 @@ jQuery(document).ready(($) => {
 	})
 })
 
+$(document).ready(function(){
+	$("a[href^='#']").on("click", function(e){
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top
+		}, 777);
+		e.preventDefault();
+		return false;
+	});
+});
+
 
 
 
